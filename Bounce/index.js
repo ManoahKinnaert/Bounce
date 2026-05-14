045144bac576dfd4bdef3c3a893fe2b36f1aae8d
Manoah Kinnaert <manoah.k@icloud.com>
2026-05-14 15:38:34 +0200

    more fun

--- Bounce/index.js
+++ Bounce/index.js
@@ -7,17 +7,32 @@ canvas.width = 600;
 canvas.height = 400;
 var ctx = canvas.getContext("2d");
 
-let ball = new Ball(15, new Position(100, 100), new Vector(-10, -3), "red");
-ball.render(canvas);
+let balls = []
 
 
-function update() {
+function spawn(radius=15, position, vector, color) {
+    let ball = new Ball(radius, position, vector, color);
+    balls.push(ball);
+}
+
+function update_balls(val, index, array) {
+    val.update(canvas);
+}
+
+function render(val, index, array) {
+    val.render(canvas);
+}
+
+function runloop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ball.update(canvas);
+    balls.forEach(update_balls)
+    balls.forEach(render);
 
-    ball.render(canvas);
-
-    requestAnimationFrame(update);
+    requestAnimationFrame(runloop);
 }
-update();
+
+spawn(15, new Position(50, 50), new Vector(-10, -10), "yellow");
+
+spawn(15, new Position(20, 50), new Vector(-16, -10), "red");
+runloop();
